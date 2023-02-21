fcf3e28555a429bb1dc6bdefe9d635a20a1327c5
joschuld <91635911+joschuld@users.noreply.github.com>
2023-02-20 19:10:27 -0600

    studio set up

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,13 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+
+    @RequestMapping("User")
+
+
+}
---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,8 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private static String username;
+    private static String email;
+    private static String password;
+}
